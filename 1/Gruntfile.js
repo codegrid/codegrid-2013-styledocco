eab527cc7691e15d71d09147954f935bcd1cea56
geckotang <macky1207@gmail.com>
2013-10-08 11:29:12 +0900

    First commit

---
+++ 1/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt){
+
+  grunt.loadNpmTasks('grunt-styleguide');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
+    styleguide: {
+      styledocco: {
+        options: {
+          name: 'Project Name',
+          framework: {
+            name: 'styledocco'
+          },
+          template: {
+            include: [
+              '_previews.js'
+            ]
+          },
+        },
+        src: [
+          'css/src/*.scss'
+        ],
+        dest: 'docs'
+      }
+    },
+    clean: {
+      styleguide: [
+        '<%= styleguide.styledocco.dest %>'
+      ]
+    }
+  });
+
+  grunt.registerTask('default', [
+    'clean',
+    'styleguide'
+  ]);
+
+};
